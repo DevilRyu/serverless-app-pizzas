56c609e1ae7ad9057de8628bb8f9c6cbc523df08
Diego Munoz <diego.munoz@kushkipagos.com>
2021-10-19 18:27:48 -0500

    Establecer un mensaje de una cola como un desencadenador de una lambda

--- handler.js
+++ handler.js
@@ -1,38 +1,41 @@
 'use strict';
 
 const {v4: uuidv4} = require('uuid');
-const AWS = require('aws-sdk');
+const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
 
-let sqs = new AWS.SQS({region: process.env.REGION});
+let sqs = new SQSClient({region: process.env.REGION});
 const QUEUE_URL = process.env.PENDING_ORDER_QUEUE;
 
 module.exports.hacerPedido = async (event, context, callback) => {
 
-    console.log('HacerPedido fue llamada');
     const orderId = uuidv4();
-
     const params = {
         MessageBody: JSON.stringify({orderId}),
         QueueUrl: QUEUE_URL
     };
 
-    sqs.sendMessage(params, function (err, data){
+    try {
 
-        if(err) {
-            sendResponse(500, err, callback);
-        }else {
+        const data = await sqs.send(new SendMessageCommand(params));
+        const message = {
+            orderId,
+            messageId: data.MessageId
+        };
+        sendResponse(200, message, callback);
 
-            const message = {
-                orderId,
-                messageId: data.MessageId
-            };
+    } catch (err) {
 
-            sendResponse(200, message, callback);
-        }
+        sendResponse(500, err, callback);
 
-    });
+    }
 
+};
 
+module.exports.prepararPedido = async (event, context, callback) => {
+
+    console.log('prepararPedido fue llamada');
+
+    callback();
 
 };
 
